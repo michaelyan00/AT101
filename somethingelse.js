963deb317e0683cf2ac7c5e5f203a222fa0a2622
michaelyan00 <92874429+michaelyan00@users.noreply.github.com>
2021-10-27 12:33:13 -0400

    added

--- somethingelse.js
+++ somethingelse.js
@@ -1,13 +1,20 @@
 $(document).ready(function() {
 //    $("p").hover(changeParStyle); 
     var nounARRAY = ["Toilet", "Antarctica", "Mars"];
+    var nounARRAY2 = ["flying", "scubadiving", "racing", "jousting"]
 
     $("button").click(function() {
         $(".noun").each(function() {
             var randomIndex = Math.floor(Math.random() * nounARRAY.length);
             $(this).text(nounARRAY[randomIndex]);
         });
-            
+
+    $("button").click(function() {
+        $(".noun2").each(function() {
+            var randomIndex = Math.floor(Math.random() * nounARRAY2.length);
+            $(this).text(nounARRAY2[randomIndex]);
+        });
+    }); 
     });
 });
     // function changeParStyle() {
